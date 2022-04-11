19a2a30c793097582e1b4c4f8b820036b607fe19
addy81 <adriana.tou@gmail.com>
2022-04-11 14:56:40 +0100

    fixed transcript filtering

--- src/code.sh
+++ src/code.sh
@@ -13,8 +13,12 @@ _annotate_vep_vcf () {
 	output_vcf="$2"
 
 	# fields to annotate with.
-	# hard coded in function for now, can be made an input but all are the same
-	#filter_fields="SYMBOL,VARIANT_CLASS,Consequence,EXON,HGVSc,HGVSp,gnomAD_AF,gnomADg_AF,CADD_PHRED,Existing_variation,ClinVar,ClinVar_CLNDN,ClinVar_CLNSIG,COSMIC,Feature"
+
+	fields="SYMBOL,VARIANT_CLASS,Consequence,EXON,HGVSc,HGVSp,gnomAD_AF,Existing_variation,Feature"
+	for entry in $(jq -r '.custom_annotations,.plugins| .[].required_fields' "$config_file_path" );
+	do
+		fields+=','$entry;
+	done
 
 	# Get number of cores/threads to use in --fork option
 	echo "Number of forks: $FORKS"
@@ -30,12 +34,35 @@ _annotate_vep_vcf () {
 	--vcf --cache --refseq --exclude_predicted --symbol --hgvs --af_gnomad \
 	--check_existing --variant_class --numbers \
 	--offline --exclude_null_alleles \
-	$ANNOTATION_STRING $PLUGIN_STRING \
+	$ANNOTATION_STRING $PLUGIN_STRING --fields "$fields" \
 	 --buffer_size "$buffer_size" --fork "$FORKS" \
 	--no_stats --compress_output bgzip --shift_3prime 1
 }
 
+_filter_vep_vcf () {
+	# Function to filter annotated VCF with VEP to retain variants with AF < 0.10 in gnomAD, for
+	# a gene symbol being present and against a given list of transcripts
 
+	# Inputs:
+	# 	$1 -> input vcf (should be output vcf of annotation)
+	# 	$2 -> name for output_vcf
+	#	$3 -> file containing one transcript per line
+
+	input_vcf="$1"
+	output_vcf="$2"
+	transcripts="$3"
+
+	# Create string filter with NM ids an no version
+	# VEP expects Feature match <transcript_id> to match partially
+	# Separate transcript separated by an "or"
+	transcript_list=$(for tr in $(less $transcripts);do echo -n "Feature match $tr\. or ";done)
+
+	/usr/bin/time -v docker run -v /home/dnanexus:/opt/vep/.vep \
+	${VEP_IMAGE_ID}  \
+	./filter_vep -i /opt/vep/.vep/"$input_vcf" \
+	-o /opt/vep/.vep/"$output_vcf" --only_matched --filter \
+	"${transcript_list%????}"
+}
 
 _format_annotation () {
     # Formats the annotation part of the command given a config file
@@ -114,33 +141,70 @@ main() {
 	mark-section "downloading inputs"
 	time dx-download-all-inputs --parallel
 
-	# array inputs end up in subdirectories (i.e. ~/in/array-input/0/), flatten to parent dir
-	find ~/in/vep_refs -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/in/vep_refs
+	# Print Config information to logs:
+	jq -r '.config_information | to_entries | .[] | .key + ": " + (.value )' "$config_file_path"
+	# Download vep resources
+	vep_refs=$(jq -r '.vep_resources | .[]' "$config_file_path")
+	xargs -P"$FORKS" -n1 dx download <<< $vep_refs
+
+	# Makes the names of the files accessible by their key name in the config
+	# Creates variables storing the name of vep_resources
+	# i.e. vep_docker=vep_v105.0.tar.gz which makes the name accessible further down
+	eval $(jq -r '.vep_resources
+		| to_entries | .[] 
+		| .key + "=$(dx describe --json " + (.value )+"
+		| jq -r '.name')" ' "$config_file_path")
+
+
+	# Download annotation files
+	echo $(date +%T)
+
+	vep_files=$(jq -r '.custom_annotations,.plugins | .[].resource_files[] | .file_id,  (.index_id // empty) ' "$config_file_path")
+	xargs -P"$FORKS" -n1 dx download <<< $vep_files
+
+	echo $(date +%T)
+
+	# Download plugin .pm files
+	if [[ ! -z $(jq '.plugins[].pm_file' "$config_file_path") ]];
+	then
+		dx download $(jq -r '.plugins[].pm_file' "$config_file_path")
+			# place plugins into plugins folder
+		mkdir ~/Plugins
+		mv ~/$plugin_config ~/Plugins/
+		mv ~/*.pm ~/Plugins/
+	else
+		echo "No plugin files found."
+	fi
+
+	mark-section "pre-annotation filtering & normalisation"
 
 	# Unpack fasta reference
-	tar xzf $fasta_tar_path
+	#fasta_tar=$(dx describe $(jq -r '.vep_resources | .' vep_testing_config_v0.0.json) --json | jq -r '.name')
+	tar xzf $ref_bcftools
 
 	# Filter by panel if provided and normalise
 	if [ "$panel_bed" ];
 	then
 		bedtools intersect -header -a "$vcf_path" -b "$panel_bed_path" \
 			| bcftools norm -f genome.fa -m -any --keep-sum AD - \
-			-o "${vcf_prefix}_filtered.vcf"
+			-o "${vcf_prefix}_normalised.vcf"
 	else
-		bcftools norm -f genome.fa -m -any --keep-sum AD  \
-			"$vcf_path" -o "${vcf_prefix}_filtered.vcf"
+		bcftools norm -f genome.fa -m -any --keep-sum AD "$vcf_path"  \
+		-o "${vcf_prefix}_normalised.vcf"
 	fi
 
-	# Download annotations
-	echo $(date +%T)
+	# If hard filters are passed in the config apply them.
+	filter_command=$(jq -r '.quality_filters' "$config_file_path")
 
-	vep_files=$(jq -r '.[] | .[].resource_files[] | .file_id,  (.index_id // empty) ' "$config_file_path")
-	xargs -P"$FORKS" -n1 dx download <<< $vep_files
-
-	echo $(date +%T)
-
-	# Download plugin .pm files
-	dx download $(jq -r '.plugins[].pm_file' "$config_file_path")
+	# Test if quality_filters variable is missing or empty in the config
+	if test -z "$filter_command" || [ $filter_command == "null" ]
+	then
+		echo "No filtering commands passed."
+		mv "${vcf_prefix}_normalised.vcf" "${vcf_prefix}_temp.vcf"
+	else
+		echo "Filter commands passed: $filter_command"
+		eval $(echo ${filter_command} "${vcf_prefix}_normalised.vcf" -o "${vcf_prefix}_temp.vcf")
+	fi
 
 	mark-section "annotating"
 
@@ -148,25 +212,20 @@ main() {
 	chmod a+rwx /home/dnanexus
 
 	# extract vep reference annotation tarball to /home/dnanexus
-	time tar xf /home/dnanexus/in/vep_refs/*.tar.gz -C /home/dnanexus
+	time tar xf $vep_cache -C /home/dnanexus
 
-	# place fasta and indexes for VEP in the annotation folder
-	chmod a+rwx /home/dnanexus/in/vep_refs/*fa.gz*
+	# Place fasta and indexes for VEP in the annotation folder
+	chmod a+rwx /home/dnanexus/*fa.gz*
 
 	# Find the annotation cache folder dynamically to allow different versions of VEP and genome to be used
 	# This will only work with refseq annotation caches currently as this is passed in the VEP command
 	# We can consider changing this if non-refseq files are required but it needs the --refseq flag to become optional
 	cache_path=$(find homo_sapiens_refseq -mindepth 1 -maxdepth 1 -type d -name "*_GRCh3*" )
 	echo "$cache_path"
-	mv /home/dnanexus/in/vep_refs/*fa.gz* ~/"${cache_path}"
-
-	# place plugins into plugins folder
-	mkdir ~/Plugins
-	mv "$vep_plugin_config_path" ~/Plugins/
-	mv ~/*.pm  ~/Plugins/
+	mv /home/dnanexus/*fa.gz* ~/"${cache_path}"
 
 	# load vep docker
-	docker load -i "$vep_docker_path"
+	docker load -i "$vep_docker"
 	VEP_IMAGE_ID=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^ensemblorg" | cut -d' ' -f2)
 
 	# Create annotation and plugin strings
@@ -174,14 +233,34 @@ main() {
 	_format_plugins "$config_file_path"
 
 	# Annotate
-	output_vcf="${vcf_prefix}_annotated.vcf.gz"
-	echo $output_vcf
-	_annotate_vep_vcf "${vcf_prefix}_filtered.vcf" "$output_vcf"
+	annotated_vcf="${vcf_prefix}_temp_annotated.vcf.gz"
+	echo $annotated_vcf
+	_annotate_vep_vcf "${vcf_prefix}_temp.vcf" "$annotated_vcf"
 
+	# Filter vcf by chosen transcript
+	output_vcf="${vcf_prefix}_annotated.vcf"
+
+	if [ "$panel_bed" ];
+	then
+		# Extract the transcripts noted in the panel bed and remove their version
+		cut -f 4 $panel_bed_path | sort | uniq |cut -d '.' -f 1 > transcripts.tsv
+		# Filter annotated output with chosen transcript
+		_filter_vep_vcf "${vcf_prefix}_temp_annotated.vcf.gz" ${output_vcf} transcripts.tsv
+		bgzip ${output_vcf}
+	elif [ "$transcript_list" ];
+	then
+		# Ensure transcript list has no versions
+		sort "$transcript_list_path" | uniq | cut -d '.' -f 1 > transcripts.tsv
+		# Filter annotated output with chosen transcript
+		_filter_vep_vcf "${vcf_prefix}_temp_annotated.vcf.gz" ${output_vcf} transcripts.tsv
+		bgzip ${output_vcf}
+	else
+		mv "${vcf_prefix}_temp_annotated.vcf.gz" "${output_vcf}.gz"
+	fi
 
 	# Upload output vcf
 	mark-section "uploading output"
-	annotated_vcf=$(dx upload $output_vcf --brief)
+	annotated_vcf=$(dx upload "${output_vcf}.gz" --brief)
 	dx-jobutil-add-output annotated_vcf "$annotated_vcf" --class=file
 
 	mark-success
